2e769669a8157276a2b1444cb40a03cb15b4a052
Your <belaneshadil@gmail.com>
2022-12-08 21:35:06 +0100

    Hadil_push

---
+++ src/main/java/FridaConfigue.java
@@ -0,0 +1,9 @@
+public class FridaConfigue{
+
+        String fridaTarget;
+        public FridaConfigue(String fileName){
+               String path = "Over-privilege-Analysis/src/main/python/";
+               fridaTarget = path+fileName;
+               return fridaTarget;
+        }
+        }
